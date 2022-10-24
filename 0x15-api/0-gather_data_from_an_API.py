1e3efb73f58f639be12cd758c0df25288debf31e
Oduo24 <geraldoduo@gmail.com>
2022-10-24 22:34:08 +0300

    Replaced key-value rerival with .get() method

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 """This module gathers data from an API"""
-from os import sys
 import requests
+from os import sys
 
-employee_id = int(sys.argv[1])
-employee_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()["name"]
-todo_list = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+if __name__ == '__main__':
 
-total_tasks = 0
-total_tasks_done = 0
-titles_of_completed_tasks = []
+    employee_id = int(sys.argv[1])
+    employee_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json().get("name")
+    todo_list = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 
-for todo in todo_list:
-    if todo["userId"] == employee_id:
-        total_tasks += 1
-        if todo["completed"] == True:
-            total_tasks_done += 1
-            titles_of_completed_tasks.append(todo["title"])
+    total_tasks = 0
+    total_tasks_done = 0
+    titles_of_completed_tasks = []
 
-
-
-print('Employee {} is done with tasks {}/{}:'.format(employee_name, total_tasks_done, total_tasks))
-
-for title in titles_of_completed_tasks:
+    for todo in todo_list:
+        if todo.get("userId") == employee_id:
+            total_tasks += 1
+            if todo.get("completed") == True:
+                total_tasks_done += 1
+                titles_of_completed_tasks.append(todo.get("title"))
+    print('Employee {} is done with tasks ({}/{}):'.format(employee_name, total_tasks_done, total_tasks))
+    for title in titles_of_completed_tasks:
     print('\t {}'.format(title))
